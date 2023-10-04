4a2adb75e0c73655bbbda3d9220bf73a144504c8
amine <amineparis97x@gmail.com>
2023-10-04 10:43:00 +0100

    ajout de regex pour vérifier la saisie de l'utilisateur sur le champ de recherche.

--- js/engine/search.js
+++ js/engine/search.js
@@ -3,43 +3,54 @@ import { displayData } from "../pages/index.js";
 const recipes = getRecipes();
 
 const mainSearch = document.querySelector(".mainSearch");
+const searchRegex = /^[a-zA-Z\s]*$/;
 
-
-
+const isValidSearchInput = (input) =>{
+    return searchRegex.test(input);
+}
 function textNormalize(word) {
-  return word.toLowerCase();
+    return word.toLowerCase();
 }
 
 // Fonction pour filtrer les recettes en fonction des critères de recherche
 const filterRecipes = () => {
-  const searchValue = textNormalize(mainSearch.value);
+    const searchValue = textNormalize(mainSearch.value);
 
-  const searchWords = searchValue.split(/\s+/).filter(word => word.length >= 3);
-  const filteredRecipes = recipes.filter(recipe => {
-    const lowerCaseName = textNormalize(recipe.name);
-    const lowerCaseDescription = textNormalize(recipe.description);
-    const matchesSearchWords = searchWords.every(word =>
-      lowerCaseName.includes(word) ||
-      lowerCaseDescription.includes(word) ||
-      recipe.ingredients.some(ingredient =>
-        textNormalize(ingredient.ingredient).includes(word)
-      )
-    );
+    const searchWords = searchValue
+        .split(/\s+/)
+        .filter((word) => word.length >= 3);
+    const filteredRecipes = recipes.filter((recipe) => {
+        const lowerCaseName = textNormalize(recipe.name);
+        const lowerCaseDescription = textNormalize(recipe.description);
+        const matchesSearchWords = searchWords.every(
+            (word) =>
+                lowerCaseName.includes(word) ||
+                lowerCaseDescription.includes(word) ||
+                recipe.ingredients.some((ingredient) =>
+                    textNormalize(ingredient.ingredient).includes(word)
+                )
+        );
 
-    return matchesSearchWords;
-  });
+        return matchesSearchWords;
+    });
 
-  if (filteredRecipes.length === 0) {
-    displayData([]);
-    return;
-  }
+    if (filteredRecipes.length === 0) {
+        displayData([]);
+        return;
+    }
 
-  displayData(filteredRecipes);
-}
+    displayData(filteredRecipes);
+};
 
 // Écoutez l'événement d'entrée dans la barre de recherche principale
 mainSearch.addEventListener("input", () => {
-  filterRecipes();
+    const searchValue = textNormalize(mainSearch.value);
+
+    if (isValidSearchInput(searchValue)) {
+        filterRecipes();
+    }else{
+        alert("le filtre n'est pas valide. Utilisez uniquement des lettres et des espaces")
+    }
 });
 
 // Appel initial pour afficher toutes les recettes
